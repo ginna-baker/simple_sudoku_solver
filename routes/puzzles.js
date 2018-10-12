218ed3a001bad200ed507183f238463db315d015
Ginna Baker <virginib@netapp.com>
2018-10-12 14:42:36 -0600

    add puzzles route

---
+++ routes/puzzles.js
@@ -0,0 +1,13 @@
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const puzzles = require('../lib/puzzles.json');
+
+/* GET a new puzzle, selected randomly from a json */
+router.get('/', function(req, res, next) {
+  const puzzle = _.sample(puzzles);
+  console.log(puzzle, 'to return');
+  res.json(puzzle);
+});
+
+module.exports = router;
